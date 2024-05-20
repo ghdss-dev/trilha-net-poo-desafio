b341bdeb99539de935f838a46d490598a1fa2361
Gustavo Henrique de Souza Silva <ghdss_ti@hotmail.com>
2024-05-20 18:19:00 -0300

    Desafio completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+class Program {
+
+    static void Main(string[] args) {
+
+        Smartphone meuIphone = new Iphone("123456789", "iPhone 12", "123456789012345", 64);
+        Smartphone meuNokia = new Nokia("987654321", "Nokia 3310", "543210987654321", 16);
+
+        meuIphone.Ligar(); 
+        meuIphone.ReceberLigacao(); 
+        meuIphone.InstalarAplicativo("WhatsApp");
+
+        meuIphone.Ligar();
+        meuNokia.ReceberLigacao(); 
+        meuNokia.InstalarAplicativo("Telegram");
+    }
+}
